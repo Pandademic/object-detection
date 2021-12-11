8333169bfe6367908ba5c7f69872c5e776c6bf0c
Pandademic <atharv.gupte510@gmail.com>
2021-12-10 20:36:36 -0500

    fix 1 error

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
     objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
-    document.getElemntById('status').innerHTML = "Status : Detecting Objects";
+    document.getElementById('status').innerHTML = "Status : Detecting Objects";
 }
 function draw(){
     image(img,0,0,640,420);//cover the canvas with image
